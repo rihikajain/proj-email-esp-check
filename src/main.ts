71f86213905278f941d299d280325f948e80d895
Rihika <116342520+rihikajain@users.noreply.github.com>
2025-08-28 12:35:50 +0530

    minor changes

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS for frontend communication
-  await app.listen(3001); // Change port to 3001
-}
-bootstrap();
